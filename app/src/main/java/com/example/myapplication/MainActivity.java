3348c4664177829d3707419f4f7200eb40b89ef8
Teerapat Kirdpaiboon <teerapat.kir@dome.tu.ac.th>
2024-09-13 14:45:32 +0700

    update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinner = findViewById(R.id.heroes_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.heroes, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(
+                android.R.layout.simple_list_item_checked
+        );
+        spinner.setAdapter(adapter);
     }
 }
